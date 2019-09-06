4b5ded0296f10e8acce6fe1f6029a5350a4d769a
TobiasWozniak <54898241+TobiasWozniak@users.noreply.github.com>
2019-09-06 09:01:01 -0500

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,16 @@
+
+/**
+ * Write a description of class HelloWorld here.
+ *
+ * @author (Tobis)
+ * @version (0)
+ */
+public class HelloWorld
+{
+    public static void main(String[] args){
+        System.out.println("hello world");
+        System.out.println("i am eternal");
+        System.out.println(args[0]);
+        
+    }
+}
